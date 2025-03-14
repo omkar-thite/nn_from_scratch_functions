192630af87e0184916b742a56c5fa2933de2e1e7
Omkar Thite <101266411+omkar-thite@users.noreply.github.com>
2025-03-14 12:35:49 +0530

    Add files via upload

---
+++ nn_from_scratch_functions.py
@@ -0,0 +1,431 @@
+def initialize_parameters(layers_dims):
+    # In accordance with lab so as to verify results
+    np.random.seed(3)
+
+    L = len(layers_dims)    
+    parameters = {}
+    for l in range(1, L):
+        parameters[f'W{l}'] = np.random.randn(layers_dims[l], layers_dims[l-1]) / np.sqrt(layers_dims[l-1])
+        parameters[f'b{l}'] = np.zeros((layers_dims[l], 1))
+        
+        assert(parameters['W' + str(l)].shape == (layers_dims[l], layers_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layers_dims[l], 1))
+    
+    '''print(f'Initialized parameters:')
+    for key in parameters:
+        print(f'{key} shape: {parameters[key].shape}')'''
+        
+    return parameters
+
+def forward_linear(A_prev, W, b):
+    Z = np.dot(W, A_prev) + b
+
+    #print(f'Calculated Z, shape: {Z.shape}')
+    
+    return Z
+
+def forward_activation(Z, activation):
+    if activation=='relu':
+        A = relu(Z)
+        #print(f'Calculated A=relu(Z), shape: {A.shape}')
+        return A
+    elif activation=='sigmoid':
+        A = sigmoid(Z)
+        #print(f'Calculated A = sigmoid(Z): {A.shape}')
+        return A
+    else:
+        return Z
+
+def cost(AL, Y, epsilon=1e-8):
+    J = - np.sum((Y * np.log(AL+ epsilon)) + (1 - Y) * np.log(1-AL + epsilon)) / Y.shape[1]
+   # print(f'Calculated J = {J}, J type: {type(J)}')
+    return np.squeeze(J)
+
+def cost_grad(AL, Y, epsilon=1e-8):
+    dAL = -(np.divide(Y, AL+epsilon) - np.divide(1 - Y, 1 - AL + epsilon))
+   # print(f'Calculated dAL, shape: {dAL.shape}')
+    return dAL
+
+def d_Z(dA, Z, activation):
+    if activation == 'relu':
+        dZ = dA * relu_prime(Z)
+        #print(f'Calculated dZ, relu layer, shape:{dZ.shape}')
+        return dZ
+    elif activation == 'sigmoid':
+        dZ = dA * sigmoid_prime(Z)
+       # print(f'Calculated dZ, sigmoid layer, shape:{dZ.shape}')
+        return dZ
+
+def relu_prime(Z):
+    g_prime = np.where(Z >= 0, 1, 0)
+    #print(f'Calculated relu prime, shape: {g_prime.shape}')
+    return g_prime
+
+def relu(Z):
+    A = np.maximum(0, Z)
+    return A
+
+def sigmoid_prime(Z):
+    a = sigmoid(Z)
+    g_prime = a * (1 - a)
+   # print(f'Calculated sigmoid prime, shape: {g_prime.shape}')    
+    return g_prime
+
+def sigmoid(Z):
+    A = 1/(1 + np.exp(-Z))
+    return A
+    
+def forward_propagation(X, parameters): 
+    caches= {}
+    A_prev = X
+    L = len(parameters)//2
+    
+    # relu params W1 to WL-1 second last layer
+    for l in range(1, L):
+        W = parameters[f'W{l}']
+        b = parameters[f'b{l}']
+        Z = forward_linear(A_prev, W, b)
+        A = forward_activation(Z, activation='relu')
+        caches[f'layer{l}'] = (A_prev, Z, W, b)
+        A_prev = A
+    
+    # for params WL-1 last layer
+    WL = parameters[f'W{L}']
+    bL = parameters[f'b{L}']
+    ZL = forward_linear(A_prev, WL, bL)
+    AL = forward_activation(ZL, activation='sigmoid')
+    caches[f'layer{L}'] = (A_prev, ZL, WL, bL) 
+    
+    return AL, caches
+
+
+def backprop_layer(dA, cache, activation):
+    m = dA.shape[1]
+    A_prev, Z, W, b = cache
+    
+    dZ = d_Z(dA, Z, activation)
+    dA_prev =  np.dot(W.T, dZ) 
+    dW = np.dot(dZ, A_prev.T) / m
+    db = np.sum(dZ,axis=1, keepdims=True) / m
+    
+    return dA_prev, dW, db
+
+def backward_propagation(AL, Y, caches):
+    #print('backprop started...')
+    
+    m = Y.shape[1]
+    grads = {}
+    L = len(caches)
+    
+    #print(f'm={m}, L={L}')
+    
+    # Sigmoid layer L
+    current_cache = caches[f'layer{L}']
+    A_prev, Z, W, b = current_cache
+    dAL = cost_grad(AL, Y)
+    grads[f'dA{L}'] = dAL
+    #print(f"grads[f'dA{L}'] done with dA{L} shape: {grads[f'dA{L}'].shape}")
+    
+    dA_prev, dW, db = backprop_layer(dAL, current_cache, activation='sigmoid')
+    grads[f'dA{L-1}'] = dA_prev
+    grads[f'dW{L}'] = dW
+    grads[f'db{L}'] = db
+    
+    for l in range(L-1, 0, -1):
+              
+        current_cache = caches[f'layer{l}']
+        A_prev, Z, W, b = current_cache 
+        dA_prev, dW, db = backprop_layer(grads[f'dA{l}'], current_cache, activation='relu')
+        grads[f'dA{l-1}'] = dA_prev
+        grads[f'dW{l}'] = dW
+        grads[f'db{l}'] = db
+    
+    return grads
+
+def update_parameters(parameters, grads, learning_rate):
+    L = len(parameters)//2
+
+    for l in range(1, L + 1):
+        parameters[f'W{l}'] -= learning_rate * grads[f'dW{l}']
+        parameters[f'b{l}'] -= learning_rate * grads[f'db{l}']
+    return parameters
+
+def L2_backpropagation(AL, Y, caches, lambd):
+    L = len(caches)
+    m = Y.shape[1]
+    grads = backward_propagation(AL, Y, caches)
+    
+    for l in range(1, L+1):
+        (A_prev, Z, W, b) = caches[f'layer{l}']
+        grads[f'dW{l}'] += (lambd/m) * W
+    return grads
+
+def L2_cost(AL, Y, parameters, lambd):
+    m = Y.shape[1]
+    L = len(parameters)//2
+    cross_entropy_cost = cost(AL, Y)
+
+    L2_regularization_cost = 0
+    for l in range(1, L+1):
+        L2_regularization_cost += np.sum(np.square(parameters[f'W{l}']))
+    
+    L2_regularization_cost = L2_regularization_cost * (lambd/(2*m))
+    
+    total_cost = cross_entropy_cost + L2_regularization_cost
+
+    return total_cost
+
+
+def model(X, Y, layers_dims, num_iters=30000, epsilon=1e-8, learning_rate=0.3, lambd=0.0, keep_prob=1):
+    costs = []
+    L = len(layers_dims)
+    L = L - 1 # remove X which isn't layer
+    m = X.shape[1]
+    
+    parameters = initialize_parameters(layers_dims)
+    
+    for i in range(num_iters):
+
+        if keep_prob == 1:
+            AL, caches = forward_propagation(X, parameters)
+        elif keep_prob < 1:
+            AL, caches = forward_propagation_with_dropout(X, parameters, keep_prob)
+
+       # AL = np.clip(AL, epsilon, 1 - epsilon)
+        
+        if lambd == 0 and keep_prob == 1:
+            grads = backward_propagation(AL, Y, caches)
+        elif lambd != 0:
+            grads = L2_backpropagation(AL, Y, caches, lambd)
+        elif keep_prob < 1:
+            grads = backward_propagation_with_dropout(AL, Y, caches, keep_prob)
+
+            
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+         # Print the loss every 10000 iterations
+        if lambd:
+            J = L2_cost(AL, Y, parameters, lambd)
+        else:
+            J = cost(AL, Y)
+            
+        if i % 10000 == 0:
+            print("Cost after iteration {}: {}".format(i, J))
+        if i % 1000 == 0:
+            costs.append(J)
+
+    # plot the cost
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (x1,000)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+
+    return parameters, grads
+
+def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
+    """
+    Update parameters using Momentum
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    v -- python dictionary containing the current velocity:
+                    v['dW' + str(l)] = ...
+                    v['db' + str(l)] = ...
+    beta -- the momentum hyperparameter, scalar
+    learning_rate -- the learning rate, scalar
+    
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    v -- python dictionary containing your updated velocities
+    """
+    L = len(parameters) // 2
+
+    for l in range(1, L+1):
+        v[f'dW{l}'] = beta * v[f'dW{l}'] + (1 - beta) * grads[f'dW{l}']
+        v[f'db{l}'] = beta * v[f'db{l}'] + (1 - beta) * grads[f'db{l}']
+
+        
+        parameters[f'W{l}'] -= learning_rate * v[f'dW{l}']
+        parameters[f'b{l}'] -= learning_rate * v[f'db{l}']
+
+    return parameters, v
+
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    """
+    Update parameters using one step of gradient descent
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters to be updated:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients to update each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    learning_rate -- the learning rate, scalar.
+    
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    """
+    L = len(parameters) // 2
+
+    for i in range(1, L+1):
+        parameters[f'W{i}'] -= learning_rate * grads[f'dW{i}']
+        parameters[f'b{i}'] -= learning_rate * grads[f'db{i}']
+    
+    return parameters
+
+def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate=0.01, 
+                                beta1=0.9, beta2=0.999, epsilon=1e-8):
+    """
+    Update parameters using Adam
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    v -- Adam variable, moving average of the first gradient, python dictionary
+    s -- Adam variable, moving average of the squared gradient, python dictionary
+    t -- Adam variable, counts the number of taken steps
+    learning_rate -- the learning rate, scalar.
+    beta1 -- Exponential decay hyperparameter for the first moment estimates 
+    beta2 -- Exponential decay hyperparameter for the second moment estimates 
+    epsilon -- hyperparameter preventing division by zero in Adam updates
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    v -- Adam variable, moving average of the first gradient, python dictionary
+    s -- Adam variable, moving average of the squared gradient, python dictionary
+    """
+    L = len(parameters) // 2
+    v_corrected = {}
+    s_corrected = {}
+
+    for l in range(1, L+1):
+        # Moving average of the gradients.
+        v['dW'+str(l)] = beta1 * v['dW'+str(l)] + (1-beta1) * grads['dW'+str(l)]
+        v['db'+str(l)] = beta1 * v['db'+str(l)] + (1-beta1) * grads['db'+str(l)]
+
+        # Compute bias-corrected first moment estimate. 
+        v_corrected['dW'+str(l)] = v['dW'+str(l)] / (1 - (beta1 ** t)) 
+        v_corrected['db'+str(l)] = v['db'+str(l)] / (1 - (beta1 ** t))
+
+        # Moving average of the squared gradients.        
+        s['dW'+str(l)] = beta2 * s['dW'+str(l)] + (1 - beta2) * (grads['dW'+str(l)] ** 2)
+        s['db'+str(l)] = beta2 * s['db'+str(l)] + (1 - beta2) * (grads['db'+str(l)] ** 2)
+        
+        # Compute bias-corrected second raw moment estimate
+        s_corrected['dW'+str(l)] = s['dW'+str(l)] / (1 - (beta2 ** t))
+        s_corrected['db'+str(l)] = s['db'+str(l)] / (1 - (beta2 ** t))
+
+        # Update parameters
+        parameters['W'+str(l)] -= learning_rate * v_corrected['dW'+str(l)]/(np.sqrt(s_corrected['dW'+str(l)]) + epsilon)
+        parameters['b'+str(l)] -= learning_rate * v_corrected['db'+str(l)]/(np.sqrt(s_corrected['db'+str(l)]) + epsilon)
+    
+    return parameters, v, s, v_corrected, s_corrected
+
+
+def model_with_lrate_decay(X, Y, layers_dims, optimizer, learning_rate = 0.0007, mini_batch_size = 64, beta = 0.9,
+          beta1 = 0.9, beta2 = 0.999,  epsilon = 1e-8, num_epochs = 5000, print_cost = True, decay=None, decay_rate=1):
+    """
+    Neural network model which can be run in different optimizer modes and includes learning rate decay
+    
+    Arguments:
+    X -- input data, of shape (2, number of examples)
+    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    layers_dims -- python list, containing the size of each layer
+    learning_rate -- the learning rate, scalar.
+    mini_batch_size -- the size of a mini batch
+    beta -- Momentum hyperparameter
+    beta1 -- Exponential decay hyperparameter for the past gradients estimates 
+    beta2 -- Exponential decay hyperparameter for the past squared gradients estimates 
+    epsilon -- hyperparameter preventing division by zero in Adam updates
+    num_epochs -- number of epochs
+    print_cost -- True to print the cost every 1000 epochs
+    decay -- function that updates learning rate 
+    decay_rate -- rate used to calculate extent of decaying in decay parameter function
+    
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    """
+
+    L = len(layers_dims)             # number of layers in the neural networks
+    costs = []                       # to keep track of the cost
+    t = 0                            # initializing the counter required for Adam update
+    seed = 10                        # For grading purposes, so that your "random" minibatches are the same as ours
+    m = X.shape[1]                   # number of training examples
+    lr_rates = []
+    learning_rate0 = learning_rate   # the original learning rate
+    
+    # Initialize parameters
+    parameters = initialize_parameters(layers_dims)
+
+    # Initialize the optimizer
+    if optimizer == "gd":
+        pass # no initialization required for gradient descent
+    elif optimizer == "momentum":
+        v = initialize_velocity(parameters)
+    elif optimizer == "adam":
+        v, s = initialize_adam(parameters)
+    
+    # Optimization loop
+    for i in range(num_epochs):
+        
+        # Define the random minibatches. We increment the seed to reshuffle differently the dataset after each epoch
+        seed = seed + 1
+        minibatches = random_mini_batches(X, Y, mini_batch_size, seed)
+        cost_total = 0 # sum of costs over all mini batches in this epoch
+        
+        for minibatch in minibatches:
+
+            # Select a minibatch
+            (minibatch_X, minibatch_Y) = minibatch
+
+            # Forward propagation
+            AL, caches = forward_propagation(minibatch_X, parameters)
+
+            # Compute cost and add to the cost total
+            cost_total += cost(AL, minibatch_Y)
+
+            # Backward propagation
+            grads = backward_propagation(AL, minibatch_Y, caches)
+
+            # Update parameters
+            if optimizer == "gd":
+                parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+            elif optimizer == "momentum":
+                parameters, v = update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
+            elif optimizer == "adam":
+                t = t + 1 # Adam counter
+                parameters, v, s, _, _ = update_parameters_with_adam(parameters, grads, v, s,
+                                                               t, learning_rate, beta1, beta2,  epsilon)
+        cost_avg = cost_total / m
+
+        if decay:
+            learning_rate = decay(learning_rate0, i, decay_rate)
+        
+        # Print the cost every 1000 epoch
+        if print_cost and i % 1000 == 0:
+            print ("Cost after epoch %i: %f" %(i, cost_avg))
+            if decay:
+                print("learning rate after epoch %i: %f"%(i, learning_rate))
+        if print_cost and i % 100 == 0:
+            costs.append(cost_avg)
+                
+    # plot the cost
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('epochs (per 100)')
+    plt.title("Learning rate = " + str(learning_rate))
+    plt.show()
+
+    return parameters
---
+++ utils.py
@@ -0,0 +1,334 @@
+def flatten_parameters(parameters):
+    """
+    Flatten parameters dictionary into a single vector.
+
+    """
+    theta = np.array([]).reshape(-1,1)
+
+    for key in parameters.keys():
+        new_vector = parameters[key].reshape((-1, 1))
+        theta = np.concatenate((theta, new_vector), axis=0)
+
+    return theta
+    
+def unflatten_parameters(theta, parameter_shapes, prefix=''):
+    """
+    Unflatten vector back into the parameters dictionary.
+    
+    Args:
+    theta (ndarray)(1,): flattened parameters
+    parameter_shapes (dict): Shapes of parameters, {W1 : W1.shape, ...} for all parameters
+    prefix (string): Prefix to apply to keys when unrolled
+    
+    Returns:
+    parameters (dict): Unflattened dictionary {dW1, db1,...}
+    """
+    parameters = {}
+    start = 0
+
+    for key in parameter_shapes.keys():
+        shape = parameter_shapes[key]
+        size = np.prod(shape)
+        parameters[f'{prefix}{key}'] = theta[start:start + size].reshape(shape)
+        start += size
+
+    return parameters
+
+def order_parameters(parameters):
+    '''
+    Orders parameters in ascending order as  dW1, db1, dW2, db2, ...
+    '''
+    tmp = {}
+    for i in range(1, (len(parameters)//2) + 1):
+        tmp[f'dW{i}'] = parameters[f'dW{i}']
+        tmp[f'db{i}'] = parameters[f'db{i}']
+    return tmp    
+    
+def compute_numerical_gradient(X, Y, 
+                               parameters, 
+                               forward_propagation, 
+                               forwardprop_args,
+                               cost_function, 
+                               cost_args, 
+                               epsilon=1e-7):
+    """
+    Compute the numerical gradient for gradient checking.
+    parameters (dict): Contains Ws and bs
+    """
+    theta = flatten_parameters(parameters)
+    #print('theta shape: ', theta.shape)
+    
+    parameter_shapes = {key: parameters[key].shape for key in parameters.keys()}
+    print(parameter_shapes)
+    
+    # # parameters including those in Ws and bs
+    num_total_parameters = theta.shape[0]
+    #print('total params: ', num_total_parameters)
+   
+    # array of numerical gradient of each parameter
+    numerical_gradients = np.zeros((num_total_parameters, 1))
+    #print('num_grad shape: ', numerical_gradients.shape)
+    
+    J_plus = np.zeros((num_total_parameters, 1))
+    J_minus = np.zeros((num_total_parameters, 1))
+
+    for i in range(num_total_parameters):
+        theta_plus = np.copy(theta)
+        theta_minus = np.copy(theta)
+
+        theta_plus[i] += epsilon
+        theta_minus[i] -= epsilon
+        
+        params_plus = unflatten_parameters(theta_plus, parameter_shapes)
+        params_minus = unflatten_parameters(theta_minus, parameter_shapes)
+
+        AL_plus, _ = forward_propagation(X, params_plus, **forwardprop_args)
+        AL_minus, _ = forward_propagation(X, params_minus, **forwardprop_args)
+
+
+        if cost_function=='l2_cost':
+            J_plus[i] = L2_cost(AL_plus, Y, params_plus, **cost_args)
+            J_minus[i] = L2_cost(AL_minus, Y, params_minus, **cost_args)
+        elif cost_function=='standard':
+            J_plus[i] = cost(AL_plus, Y, **cost_args)
+            J_minus[i] = cost(AL_minus, Y, **cost_args)
+        
+        numerical_gradients[i] = (J_plus[i] - J_minus[i]) / (2*epsilon)
+
+    #print('final num_grads shape: ', numerical_gradients.shape)
+    return unflatten_parameters(numerical_gradients, parameter_shapes, prefix='d')
+
+
+def gradient_checking(X, Y, 
+                      parameters, 
+                      forward_propagation,
+                      backward_propagation, 
+                      forwardprop_args={},
+                      backprop_args = {},
+                      cost_function='standard', 
+                      cost_args={}):
+
+    gradapprox = compute_numerical_gradient(X, Y, 
+                                            parameters, 
+                                            forward_propagation, 
+                                            forwardprop_args,
+                                            cost_function, 
+                                            cost_args)
+    
+    print(f'gradaaprox keys: {gradapprox.keys()}')
+    
+    AL, caches = forward_propagation(X, parameters, **forwardprop_args)
+    grads = backward_propagation(AL, Y, caches, **backprop_args)
+
+    # Filter out non-parameter gradients dA
+    keys_to_remove = [key for key in grads if key.startswith("dA")]
+    for key in keys_to_remove:
+        del grads[key]
+        
+
+    # order of keys are different in grads and gradapprox. imrove this later.
+    # grads store derivatives in reverse order due to backprop db3, dW3, ...
+    # Order keys of grads before flattening it
+    grads = order_parameters(grads)
+    print(f'grads keys: {grads.keys()}')
+  
+    gradapprox = flatten_parameters(gradapprox)
+    grads = flatten_parameters(grads)
+    print('flattened grads shape: ', grads.shape)
+    print('flattened gradapprox shape: ', gradapprox.shape)
+
+
+    # Difference
+    numerator = np.linalg.norm(grads - gradapprox)
+    denominator = np.linalg.norm(grads) + np.linalg.norm(gradapprox)
+    difference = numerator / denominator
+
+    if True:
+        
+        if difference > 2e-7:
+            print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+        else:
+            print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+
+    return difference
+
+def predict(X, y, parameters):
+    """
+    This function is used to predict the results of a  n-layer neural network.
+    Args:
+    X (ndarray): data set of examples you would like to label
+    parameters -- parameters of the trained model
+    
+    Returns:
+    p -- predictions for the given dataset X
+    """
+    m = X.shape[1]
+    L = len(parameters)//2
+    AL, caches = forward_propagation(X, parameters)
+    p = np.int64(AL > 0.5)
+    print(f'Accuracy: {np.mean(p == y)}')
+    
+    return p
+
+def random_mini_batches(X, Y, mini_batch_size=64, seed=0):
+        """
+        Creates a list of random minibatches from (X, Y)
+        
+        Arguments:
+        X -- input data, of shape (input size, number of examples)
+        Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+        mini_batch_size -- size of the mini-batches, integer
+        
+        Returns:
+        mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+        """
+        np.random.seed(seed)  # Grading purspose
+        
+        m = X.shape[1]
+        mini_batches = []
+    
+        # Shuffle
+        indices = list(np.random.permutation(m))
+        X = X[:, indices]
+        Y = Y[:, indices].reshape((1,m))
+    
+        inc = mini_batch_size
+    
+        # Partition
+        num_complete_mini_batches = math.floor(m/inc)
+    
+        for k in range(num_complete_mini_batches):
+            mini_batch_X = X[:, k * inc : (k + 1) * inc]
+            mini_batch_Y = Y[:, k * inc : (k + 1) * inc]
+            mini_batch = (mini_batch_X, mini_batch_Y)
+            mini_batches.append(mini_batch)
+    
+        # end case where last batch has < mini_batch_size examples
+        mini_batch_X = X[:, num_complete_mini_batches * mini_batch_size : m]
+        mini_batch_Y = Y[:, num_complete_mini_batches * mini_batch_size : m]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+        return mini_batches
+
+def initialize_velocity(parameters):
+    """
+    Initializes the velocity as a python dictionary with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL" 
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    
+    Returns:
+    v -- python dictionary containing the current velocity.
+                    v['dW' + str(l)] = velocity of dWl
+                    v['db' + str(l)] = velocity of dbl
+    """
+
+    L = len(parameters) // 2
+    v = {}
+
+    for l in range(1, L+1):
+        v[f'dW{l}'] = np.zeros(parameters[f'W{l}'].shape)
+        v[f'db{l}'] = np.zeros(parameters[f'b{l}'].shape)
+    
+    return v
+
+def initialize_adam(parameters):
+    """
+    Initializes v and s as two python dictionaries with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL" 
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters["W" + str(l)] = Wl
+                    parameters["b" + str(l)] = bl
+    
+    Returns: 
+    v -- python dictionary that will contain the exponentially weighted average of the gradient. Initialized with zeros.
+                    v["dW" + str(l)] = ...
+                    v["db" + str(l)] = ...
+    s -- python dictionary that will contain the exponentially weighted average of the squared gradient. Initialized with zeros.
+                    s["dW" + str(l)] = ...
+                    s["db" + str(l)] = ...
+
+    """
+    L = len(parameters) // 2
+    v = {}
+    s = {}
+
+    for l in range(1, L+1):
+        v['dW' + str(l)] = np.zeros(parameters['W' + str(l)].shape)
+        v['db' + str(l)] = np.zeros(parameters['b' + str(l)].shape)
+
+        s['dW' + str(l)] = np.zeros(parameters['W' + str(l)].shape)
+        s['db' + str(l)] = np.zeros(parameters['b' + str(l)].shape)
+
+    return v, s
+
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+    
+def predict_dec(parameters, X):
+    """
+    Used for plotting decision boundary.
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters 
+    X -- input data of size (m, K)
+    
+    Returns
+    predictions -- vector of predictions of our model (red: 0 / blue: 1)
+    """
+    
+    # Predict using forward propagation and a classification threshold of 0.5
+    AL, cache = forward_propagation(X, parameters)
+    predictions = (AL > 0.5)
+    return predictions
+
+def update_lr(learning_rate0, epoch_num, decay_rate):
+    """
+    Calculates updated the learning rate using exponential weight decay.
+    
+    Arguments:
+    learning_rate0 -- Original learning rate. Scalar
+    epoch_num -- Epoch number. Integer
+    decay_rate -- Decay rate. Scalar
+
+    Returns:
+    learning_rate -- Updated learning rate. Scalar 
+    """
+    return learning_rate0 * (1 / (1 + (decay_rate * epoch_num)))
+
+def schedule_lr_decay(learning_rate0, epoch_num, decay_rate, timeInterval=1000):
+    """
+    Calculates updated the learning rate using exponential weight decay.
+    
+    Arguments:
+    learning_rate0 -- Original learning rate. Scalar
+    epoch_num -- Epoch number. Integer.
+    decay_rate -- Decay rate. Scalar.
+    time_interval -- Number of epochs where you update the learning rate.
+
+    Returns:
+    learning_rate -- Updated learning rate. Scalar 
+    """
+
+    return learning_rate0 / (1 + (decay_rate * (np.floor(epoch_num/timeInterval))))
